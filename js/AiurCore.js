7e88eeec9a4496811740182b8e6c3ce5d5ff1878
Anduin2017 <Anduin@aiursoft.com>
2018-10-22 00:02:08 +0800

    Create jquery-utc-time

--- js/AiurCore.js
+++ js/AiurCore.js
@@ -1,3 +1,44 @@
+
+// Temp put jquery-utc-time here.
+$(document).ready(function () {
+    var timeSince = function (date) {
+        var seconds = Math.floor((new Date() - date) / 1000);
+        var interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return date.toLocaleDateString();;
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days ago";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours ago";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes ago";
+        }
+        return Math.floor(seconds) + " seconds ago";
+    }
+    $('*[data-utc-time]').each(function (t) {
+        var timefield = $(this);
+        var timevalue = timefield.attr('data-utc-time');
+        if (!timevalue.endsWith(' UTC')) {
+            timevalue = timevalue + ' UTC';
+        }
+        var date = new Date(timevalue);
+        var text = timeSince(date);
+        timefield.html(text);
+        if (timefield.tooltip) {
+            timefield.attr('data-toggle', 'tooltip');
+            timefield.attr('data-trigger', 'hover');
+            timefield.attr('data-title', date.toLocaleString());
+            timefield.tooltip();
+        }
+    });
+});
+
 $(document).ready(function () {
     // Activate clipboard tool
     new ClipboardJS('[data-clipboard-text]');
@@ -9,14 +50,6 @@ $(document).ready(function () {
             $('[data-toggle="tooltip"]').tooltip('hide');
         }, 2000);
     });
-
-    //Seems useless
-    // $.fn.top = function () {
-    //     $('html, body').animate({
-    //         scrollTop: $(this).offset().top + 'px'
-    //     }, 'medium');
-    //     return this;
-    // }
 
     //Aiur Scroll to top
     $(document).scroll(function () {
