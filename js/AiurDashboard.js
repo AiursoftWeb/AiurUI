9aaa09e638827664f032fd3cceb6308c2916adf4
Anduin Xue <anduin@aiursoft.com>
2020-05-17 16:08:01 +0800

    Fix nav scroll.

--- js/AiurDashboard.js
+++ js/AiurDashboard.js
@@ -7,20 +7,17 @@ $(function () {
         "pageLength": 15
     });
 
-    $("#sidenavToggler").click(function(e) {
+    $("#sidenavToggler").click(function (e) {
         e.preventDefault();
         $("body").toggleClass("sidenav-toggled");
         $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
         $(".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level").removeClass("show");
     });
 
-    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
-        if ($(window).width() > 768) {
-            var e0 = e.originalEvent,
-                delta = e0.wheelDelta || -e0.detail;
-            this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-            e.preventDefault();
-        }
+    $('body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse').on('mousewheel DOMMouseScroll', function (e) {
+        var originalEvent = e.originalEvent;
+        var delta = originalEvent.wheelDelta || -originalEvent.detail;
+        this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+        e.preventDefault();
     });
 });
