91732ae511193ab99baaaf7728b54df55046e258
Anduin Xue <anduin@aiursoft.com>
2020-05-21 00:14:14 +0800

    Convert to scss.

--- gulpfile.js
+++ gulpfile.js
@@ -15,48 +15,6 @@ var supportedfonts = [
     'node_modules/**/fonts/*.otf'
 ];
 
-var packages = [
-    // AiurCore - CSS
-    {
-        inputFiles: [
-            'node_modules/bootstrap/dist/css/bootstrap.css',
-            'node_modules/font-awesome/css/font-awesome.css',
-            'node_modules/nprogress/nprogress.css',
-            'css/AiurCore.css'
-        ],
-        iscss: true,
-        outputFileName: 'AiurCore.min.css'
-    },
-    // AiurMarket - CSS
-    {
-        inputFiles: [
-            'css/AiurMarket.css'
-        ],
-        iscss: true,
-        outputFileName: 'AiurMarket.min.css'
-    },
-    // AiurProduct - CSS
-    {
-        inputFiles: [
-            'node_modules/primer/build/build.css',
-            'css/AiurProduct.css'
-        ],
-        iscss: true,
-        outputFileName: 'AiurProduct.min.css'
-    },
-    // AiurDashboard - CSS
-    {
-        inputFiles: [
-            'node_modules/datatables/media/css/jquery.dataTables.css',
-            'node_modules/primer-markdown/build/build.css',
-            'node_modules/github-syntax-light/lib/github-light.css',
-            'node_modules/dropify/dist/css/dropify.min.css',
-            'css/AiurDashboard.css'
-        ],
-        iscss: true,
-        outputFileName: 'AiurDashboard.min.css'
-    }
-]
 
 gulp.task('clean', function (done) {
     del('dist/**/*');
@@ -70,17 +28,5 @@ gulp.task('copy-fonts', function () {
         .pipe(gulp.dest('fonts'));
 })
 
-gulp.task('css', function (done) {
-    packages.filter(t => t.iscss).forEach(function (package) {
-        gulp.src(package.inputFiles)
-            .pipe(expect(package.inputFiles))
-            .pipe(concat('temp'))
-            .pipe(cleancss())
-            .pipe(rename(package.outputFileName))
-            .pipe(gulp.dest('dist'));
-    });
-    done();
-});
-
-gulp.task('bundle', gulp.series('clean', 'copy-fonts', 'css'));
+gulp.task('bundle', gulp.series('clean', 'copy-fonts'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+var files = ['AiurCore', 'AiurDashboard', 'AiurProduct', 'AiurMarket'];
 
-module.exports = [
-  {
+var js = files.map(f => {
+  return {
     mode: "production",
-    entry: {
-      'AiurCore.js': [
-        path.resolve(__dirname, 'js/AiurCore.js'),
-      ]
-    },
+    entry: path.resolve(__dirname, `js/${f}.js`),
     output: {
-      filename: 'AiurCore.min.js',
+      filename: `${f}.min.js`,
       path: path.resolve(__dirname, 'dist'),
       libraryTarget: 'window',
       libraryExport: 'default'
@@ -21,62 +18,35 @@ module.exports = [
         new TerserPlugin({})
       ]
     }
-  },
-  {
-    mode: "production",
-    entry: {
-      'AiurDashboard.js': [
-        path.resolve(__dirname, 'js/AiurDashboard.js'),
-      ]
-    },
-    output: {
-      filename: 'AiurDashboard.min.js',
-      path: path.resolve(__dirname, 'dist'),
-      libraryTarget: 'window'
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({})
-      ]
-    }
-  },
-  {
-    mode: "production",
-    entry: {
-      'AiurMarket.js': [
-        path.resolve(__dirname, 'js/AiurMarket.js'),
-      ]
-    },
-    output: {
-      filename: 'AiurMarket.min.js',
-      path: path.resolve(__dirname, 'dist'),
-      libraryTarget: 'window'
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({})
-      ]
-    }
-  },
-  {
-    mode: "production",
-    entry: {
-      'AiurProduct.js': [
-        path.resolve(__dirname, 'js/AiurProduct.js'),
-      ]
-    },
-    output: {
-      filename: 'AiurProduct.min.js',
-      path: path.resolve(__dirname, 'dist'),
-      libraryTarget: 'window'
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({})
+  }
+});
+var css = files.map(f => {
+  return{
+    mode: 'production',
+    entry: path.resolve(__dirname, `css/${f}.scss`),
+    module: {
+      rules: [
+        {
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].min.css',
+              }
+            },
+            {
+              loader: 'extract-loader'
+            },
+            {
+              loader: 'css-loader?-url'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        }
       ]
     }
   }
-];
+})
+module.exports = js.concat(css);
