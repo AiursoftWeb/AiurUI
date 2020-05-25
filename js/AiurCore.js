8dcc29acca85f875b27999f079ca45c9414a45e6
Anduin Xue <anduin@aiursoft.com>
2020-05-25 10:38:47 +0800

    New way to init ui.

--- js/AiurCore.js
+++ js/AiurCore.js
@@ -11,38 +11,47 @@ import DarkMonitor from './src/Dark';
 window.$ = $;
 window.jQuery = $;
 
-new DarkMonitor();
-
-(function ($) {
-
-    // Activate clipboard tool
-    new Clipboard('[data-clipboard-text]', {});
-
-    new DisableWith('data-disable-with');
-
-    new UtcTime({
-        onSet: function (element) {
-            $(element).tooltip();
-        }
-    });
-
-    new Clickable('data-href');
-
-    // Activate tooltip tool
-    $('[data-toggle="tooltip"]').tooltip();
-    $('[data-toggle="tooltip"]').on('click', function () {
-        setTimeout(function () {
-            $('[data-toggle="tooltip"]').tooltip('hide');
-        }, 2000);
-    });
-
-    var setLanguageLink = function () {
-        var link = $('[data-language-change-link]').attr('href');
-        var host = encodeURIComponent(window.location.origin);
-        var path = encodeURIComponent(window.location.pathname + window.location.search);
-        link = link + "?host=" + host + "&path=" + path;
-        $('[data-language-change-link]').attr("href", link);
+class UIInitor {
+    constructor() {
+        this.initUI();
+        window.addEventListener('load', this.initDarkTheme);
     }
 
-    setLanguageLink();
-})(jQuery);
+    initUI() {
+        new DarkMonitor();
+
+        // Activate clipboard tool
+        new Clipboard('[data-clipboard-text]', {});
+
+        new DisableWith('data-disable-with');
+
+        new UtcTime({
+            onSet: function (element) {
+                $(element).tooltip();
+            }
+        });
+
+        new Clickable('data-href');
+
+        // Activate tooltip tool
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="tooltip"]').on('click', function () {
+            setTimeout(function () {
+                $('[data-toggle="tooltip"]').tooltip('hide');
+            }, 2000);
+        });
+
+        var setLanguageLink = function () {
+            var link = $('[data-language-change-link]').attr('href');
+            var host = encodeURIComponent(window.location.origin);
+            var path = encodeURIComponent(window.location.pathname + window.location.search);
+            link = link + "?host=" + host + "&path=" + path;
+            $('[data-language-change-link]').attr("href", link);
+        }
+
+        setLanguageLink();
+
+    }
+}
+
+new UIInitor();
