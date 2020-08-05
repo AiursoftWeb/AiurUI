6aea1f01dcafe2dc3c677611498caa99e85dbf6a
Anduin Xue <xuef@microsoft.com>
2020-08-05 13:16:19 +0800

    Fix a bash grammar mistake.

--- install.sh
+++ install.sh
@@ -36,11 +36,11 @@ add_caddy()
     domain_name="$1"
     path="$2"
     cat /etc/caddy/Caddyfile | grep -q "an easy way" && echo "" > /etc/caddy/Caddyfile
-    echo '
+    echo "
 $domain_name {
     root * $path
     file_server
-}' >> /etc/caddy/Caddyfile
+}" >> /etc/caddy/Caddyfile
     systemctl restart caddy.service
 }
 
